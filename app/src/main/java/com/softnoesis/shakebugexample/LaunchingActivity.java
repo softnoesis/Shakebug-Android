2b678e7e4aa91c8f77fb716ccd3391697d55d7d1
Android Developer <>
2021-11-27 13:16:30 +0530

    Shake Bug Implemented

--- app/src/main/java/com/softnoesis/shakebugexample/LaunchingActivity.java
+++ app/src/main/java/com/softnoesis/shakebugexample/LaunchingActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.softnoesis.shakebuglibrary.ShakeBugInitialize;
+
+public class LaunchingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new ShakeBugInitialize(this,"App_Token");
     }
 }
