edc84920ee0b21f6800047af65d97cc89a0dc0a0
webappdev1990 <94592557+webappdev1990@users.noreply.github.com>
2023-03-15 14:42:12 +0530

    ShakeBug instance  implemented

--- app/src/main/java/com/softnoesis/shakebugexample/LaunchingActivity.java
+++ app/src/main/java/com/softnoesis/shakebugexample/LaunchingActivity.java
@@ -12,6 +12,7 @@ public class LaunchingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new ShakeBugInitialize(this,"App_Token");
+        ShakeBug.sharedInstance().initiateWithKey(this, "Your_Key");
+        ShakeBug.sharedInstance().showSplashScreen(true);    
     }
 }
